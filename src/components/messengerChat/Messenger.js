5996602ff27bc0f21db3877f88b84c06fadb9201
QuatBaThien <86465894+QuatBaThien@users.noreply.github.com>
2023-04-17 15:22:56 +0700

    [F] Fix messchat pos

---
+++ src/components/messengerChat/Messenger.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+export default function Messenger(props) {
+    return (
+      <div className="messenger-chat-div">
+          <MessengerCustomerChat
+              pageId="106732379067322"
+              appId="925920615323812"
+           />,
+      </div>
+    );
+}
--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -3,8 +3,9 @@ import Header from "../../components/header/Header";
 import Greeting from "../../containers/greeting/Greeting";
 import Skills from "../../containers/skills/Skills";
 import Footer from "../../components/footer/Footer";
+import Messenger from "../../components/messengerChat/Messenger"
 import TopButton from "../../components/topButton/TopButton";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 class Home extends Component {
   render() {
@@ -15,10 +16,7 @@ class Home extends Component {
         <Skills theme={this.props.theme} />
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
-        <MessengerCustomerChat
-              pageId="106732379067322"
-              appId="925920615323812"
-           />,
+        <Messenger theme={this.props.theme}/>,
       </div>
     );
   }
